e7e2e117fcd75c2cbfd7786f2efc4ac46d3acfd4
Bandita69 <aaandris96@gmail.com>
2022-03-22 15:11:23 +0100

    teszt

--- HZS05V_0307/hzs05v6fel2.R
+++ HZS05V_0307/hzs05v6fel2.R
@@ -3,9 +3,9 @@
 
 library(ggplot2)
   
-data <- data.frame(name = c('P1', 'P2', 'P3', 'P4', 'P5'), 
-                   start = c(2, 5, 0, 18, 13),
-                   end = c(5, 13, 2, 38, 18)
+data <- data.frame(name = c('P1', 'P2', 'P3', 'P4'), 
+                   start = c(2, 5, 0, 18),
+                   end = c(5, 13, 2, 38)
 )
 
 ggplot(data, aes(x=start, xend=end, y=name, yend=name)) +
