a5eaac4e3abf0eaf5daf57b18d5073eb400189b7
Bandita69 <aaandris96@gmail.com>
2022-05-01 13:33:05 +0200

    Update hzs05v.c

--- OSSemTask_HZS05V/hzs05v.c
+++ OSSemTask_HZS05V/hzs05v.c
@@ -3,22 +3,28 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
- 
-int main(void) {
-  for(int i = 1; i <= 5; i++) {
-    pid_t pid = fork();
- 
-    if(pid == 0) {
-      printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
-      exit(0);
+
+int main(void)
+{
+    pid_t pid_par = fork();
+
+    for (int i = 1; i <= 3; i++)
+    {
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+            printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
+            exit(0);
+        }
+        else
+        {
+            printf("Parent process => PID=%d\n", getpid());
+            printf("Waiting for child processes to finish...\n");
+            wait(NULL);
+            printf("child process finished.\n");
+        }
     }
-    else  {
-      printf("Parent process => PID=%d\n", getpid());
-      printf("Waiting for child processes to finish...\n");
-      wait(NULL);
-      printf("child process finished.\n");
-    }
-  }
- 
-  return EXIT_SUCCESS;
+
+    return EXIT_SUCCESS;
 }
