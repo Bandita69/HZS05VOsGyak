db8db0bf5ed5f328d33c740d0d21b4417eb71b52
Bandita69 <aaandris96@gmail.com>
2022-05-01 13:40:03 +0200

    Update hzs05v.c

--- OSSemTask_HZS05V/hzs05v.c
+++ OSSemTask_HZS05V/hzs05v.c
@@ -6,32 +6,37 @@
 
 int main(void)
 {
-    pid_t pid_par = fork();
 
-    if (pid_par != 0)
+    for (size_t i = 0; i < 2; i++)
     {
-        printf("szulo\n");
-    }
-    else
-    {
-        for (int i = 1; i <= 3; i++)
+        pid_t pid_par = fork();
+
+        if (pid_par != 0)
         {
-            pid_t pid = fork();
-
-            if (pid == 0)
-            {
-                printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
-                exit(0);
-            }
-            else
-            {
-                printf("Parent process => PID=%d\n", getpid());
-                printf("Waiting for child processes to finish...\n");
-                wait(NULL);
-                printf("child process finished.\n");
-            }
+            printf("szulo\n");
+            exit(0);
         }
-        exit(0);
+        else
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                pid_t pid = fork();
+
+                if (pid == 0)
+                {
+                    printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
+                    exit(0);
+                }
+                else
+                {
+                    printf("Parent process => PID=%d\n", getpid());
+                    printf("Waiting for child processes to finish...\n");
+                    wait(NULL);
+                    printf("child process finished.\n");
+                }
+            }
+            exit(0);
+        }
     }
 
     return EXIT_SUCCESS;
