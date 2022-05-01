aca3bda40dd43a7c224812d61bf15e2a1d181ee0
Bandita69 <aaandris96@gmail.com>
2022-05-01 13:16:07 +0200

    Update hzs05v.c

--- OSSemTask_HZS05V/hzs05v.c
+++ OSSemTask_HZS05V/hzs05v.c
@@ -2,81 +2,23 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
 #include <unistd.h>
-#include <fcntl.h>
-
-int main()
-{
-
-
-    pid_t child_pid, wpid;
-    pid_t grandson_pid;
-    int status = 0;
-    
-    int grandson = 4;
-
-
-    printf("Fo procesz [Procesz id: %d].\n\n",getpid());
-
-    
-        
-	child_pid = fork();
-        if (child_pid > 0)
-        {
-
-            //szolo kod
-        
-    	   printf("\n szulo megszuletett [Procesz id: %d][Szulo Szulo Procesz id: %d].\n ",getpid(),getppid());
-	
-
-            for (int id2=0; id2<grandson; id2++)
-            {
-		
-                grandson_pid = fork();
-                if (grandson_pid > 0)
-                {
-			
-                    //unoka kod
-                    printf("\n(unoka)gyerek megszuletett [Procesz id: %d] [Szulo Procesz id: %d]-tol.\n ",getpid(),getppid());
-			sleep(2);
-                  }
- 		}
- 	      }
-
-
-	child_pid = fork();
-        if (child_pid > 0)
-        {
-            //szolo kod
-        
-    	   printf("\n szulo megszuletett [Procesz id: %d][Szulo Szulo Procesz id: %d].\n ",getpid(),getppid());
-	
-
-            for (int id2=0; id2<grandson; id2++)
-            {
-		
-                grandson_pid = fork();
-                if (grandson_pid > 0)
-                {
-			
-                    //unoka kod
-                    printf("\n(unoka)gyerek megszuletett [Procesz id: %d] [Szulo Procesz id: %d]-tol.\n ",getpid(),getppid());
-			sleep(3);
-                  }
- 		}
- 	      }
-
-
-
-        printf("\n a gyerek megszuntek a [Szulo Procesz id: %d] most szunik meg.\n\n",getpid());
-
-
-    
-
-
-     // megvarja a nagyszulo a gyerekeket es csak azutan fut le:
-    
-    kill(child_pid, SIGKILL);
-    printf("\n(nagy)szulo megszunt letezni [Procesz id: %d].\n",getpid());
+ 
+int main(void) {
+  for(int i = 1; i <= 5; i++) {
+    pid_t pid = fork();
+ 
+    if(pid == 0) {
+      printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
+      exit(0);
+    }
+    else  {
+      printf("Parent process => PID=%d\n", getpid());
+      printf("Waiting for child processes to finish...\n");
+      wait(NULL);
+      printf("child process finished.\n");
+    }
+  }
+ 
+  return EXIT_SUCCESS;
 }
