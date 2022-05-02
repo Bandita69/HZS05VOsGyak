e413e30c1ea06a9a3d56785d3085582504086813
Bandita69 <aaandris96@gmail.com>
2022-05-02 17:30:13 +0200

    12 frissitese

--- HZS05V_0502/hzs05v_12_1.cpp
+++ HZS05V_0502/hzs05v_12_1.cpp
@@ -2,9 +2,9 @@
 // Memóriakeret (igényelt lapok/capacity): <egy szam>
 // Fifo laphibak szamanak megkeresese
 #include <bits/stdc++.h>
-#include<iostream>
-#include<cstring>
-#include<sstream>
+#include <iostream>
+#include <cstring>
+#include <sstream>
 using namespace std;
 int pageFaultsLRU(int pages[], int n, int capacity)
 {
@@ -64,178 +64,165 @@ int pageFaultsLRU(int pages[], int n, int capacity)
     return page_faults;
 }
 
-bool search(int key, vector<int>& fr)
+bool search(int key, vector<int> &fr)
 {
     for (int i = 0; i < fr.size(); i++)
         if (fr[i] == key)
             return true;
     return false;
 }
- 
+
 // Function to find the frame that will not be used
 // recently in future after given index in pg[0..pn-1]
-int predict(int pg[], vector<int>& fr, int pn, int index)
+int predict(int pg[], vector<int> &fr, int pn, int index)
 {
     // Store the index of pages which are going
     // to be used recently in future
     int res = -1, farthest = index;
-    for (int i = 0; i < fr.size(); i++) {
+    for (int i = 0; i < fr.size(); i++)
+    {
         int j;
-        for (j = index; j < pn; j++) {
-            if (fr[i] == pg[j]) {
-                if (j > farthest) {
+        for (j = index; j < pn; j++)
+        {
+            if (fr[i] == pg[j])
+            {
+                if (j > farthest)
+                {
                     farthest = j;
                     res = i;
                 }
                 break;
             }
         }
- 
+
         // If a page is never referenced in future,
         // return it.
         if (j == pn)
             return i;
     }
- 
+
     // If all of the frames were not in future,
     // return any of them, we return 0. Otherwise
     // we return res.
     return (res == -1) ? 0 : res;
 }
- 
-int pageFaultsOPT(int pg[], int pn, int fn)
+
+int pageFaultsOPT(int pages[], int n, int capacity)
 {
-    // Create an array for given number of
-    // frames and initialize it as empty.
-    vector<int> fr;
- 
-    // Traverse through page reference array
-    // and check for miss and hit.
+
+    vector<int> frames;
+
+
     int hit = 0;
-    for (int i = 0; i < pn; i++) {
- 
-        // Page found in a frame : HIT
-        if (search(pg[i], fr)) {
+    for (int i = 0; i < n; i++)
+    {
+
+        // a hit azt jelenti hogy talalt ilyen a lapok kozott
+        if (search(pages[i], frames))
+        {
             hit++;
             continue;
         }
- 
-        // Page not found in a frame : MISS
- 
-        // If there is space available in frames.
-        if (fr.size() < fn)
-            fr.push_back(pg[i]);
- 
-        // Find the page to be replaced.
-        else {
-            int j = predict(pg, fr, pn, i + 1);
-            fr[j] = pg[i];
+
+        // a miss az azt hogy nem, vagyis laphiba
+
+        // ha van hely uj lap szamara
+        if (frames.size() < capacity)
+            frames.push_back(pages[i]);
+
+        // megkeressuk hogy melyikett csereljuk
+        else
+        {
+            int j = predict(pages, frames, n, i + 1);
+            frames[j] = pages[i];
         }
     }
-    return(pn-hit);
+    return (n - hit);
 }
 
-// If page found, updates the second chance bit to true
- bool findAndUpdate(int x,int arr[],
-                bool second_chance[],int frames)
- 
+bool findAndUpdate(int x, int arr[],
+                   bool second_chance[], int frames)
+
 {
     int i;
-     
-    for(i = 0; i < frames; i++)
+
+    for (i = 0; i < frames; i++)
     {
-         
-        if(arr[i] == x)
+
+        if (arr[i] == x)
         {
-            // Mark that the page deserves a second chance
+            // Megjeloli a masodik eselyre alkalmas lapokat, az igaz jelueket meg nem kell cserelni
             second_chance[i] = true;
-             
-            // Return 'true', that is there was a hit
-            // and so there's no need to replace any page
+
             return true;
         }
     }
-     
-    // Return 'false' so that a page for replacement is selected
-    // as he reuested page doesn't exist in memory
+
+    // hamissal ter vissza ha nincs tobb eselye a lapnak
     return false;
-     
 }
- 
- 
-// Updates the page in memory and returns the pointer
- int replaceAndUpdate(int x,int arr[],
-            bool second_chance[],int frames,int pointer)
+
+int replaceAndUpdate(int x, int arr[],
+                     bool second_chance[], int frames, int pointer)
 {
-    while(true)
+    while (true)
     {
-         
-        // We found the page to replace
-        if(!second_chance[pointer])
+
+        // Ha talalt kicsrelni valot
+        if (!second_chance[pointer])
         {
-            // Replace with new page
+            // Kicsereli
             arr[pointer] = x;
-             
-            // Return updated pointer
+
+            // visszater a frissitett mutatoval
             return (pointer + 1) % frames;
         }
-         
-        // Mark it 'false' as it got one chance
-        // and will be replaced next time unless accessed again
+
+        // ha false akkor kovetkezonek ot cserelik
         second_chance[pointer] = false;
-         
-        //Pointer is updated in round robin manner
+
+        // round robin stilusu mutato frissites
         pointer = (pointer + 1) % frames;
     }
 }
- 
+
 int pageFaultsSC(int pages[], int n, int capacity)
 {
-    int pointer, i, l=0, x, pf;
-     
-    //initially we consider frame 0 is to be replaced
-    pointer = 0;
-     
-    //number of page faults
-    pf = 0;
-     
-    // Create a array to hold page numbers
-    int arr[capacity];
-     
-    // No pages initially in frame,
-    // which is indicated by -1
-    memset(arr, -1, sizeof(arr));
-     
-    // Create second chance array.
-    // Can also be a byte array for optimizing memory
-    bool second_chance[capacity];
-     
+    int pointer = 0, i, l = 0, x, pageFaults;
 
-     
-    for(i = 0; i < n; i++)
+    pageFaults = 0;
+
+    // lap szamok tarolo
+    int arr[capacity];
+
+    // -1 azt jelenti hogy ures lista, mert eredetileg nincs benne semmi
+    memset(arr, -1, sizeof(arr));
+
+    // second chance keret.
+
+    bool second_chance[capacity];
+
+    for (i = 0; i < n; i++)
     {
         x = pages[i];
-         
-        // Finds if there exists a need to replace
-        // any page at all
-        if(!findAndUpdate(x,arr,second_chance,capacity))
+        // megnezi melyik lapot kell firssiteni
+        if (!findAndUpdate(x, arr, second_chance, capacity))
         {
-            // Selects and updates a victim page
-            pointer = replaceAndUpdate(x,arr,
-                    second_chance,capacity,pointer);
-             
-            // Update page faults
-            pf++;
+            // frissiti a lapot
+            pointer = replaceAndUpdate(x, arr,
+                                       second_chance, capacity, pointer);
+
+            pageFaults++;
         }
     }
-    return pf;
+    return pageFaults;
 }
 
 int pageFaultsFifo(int pages[], int n, int capacity)
 {
 
     unordered_set<int> s;
-    //egy beepitett fifo alapu tarolo
+    // egy beepitett fifo alapu tarolo
     queue<int> indexes;
 
     // elso lappal kezdve, vegig nezi hogy tud e lapokat tarolni meg
@@ -298,17 +285,16 @@ int main()
     cout << pageFaultsSC(pages, n, capacity) << endl;
 
     cout << "\nUgyan az 4 memoria igennyel" << endl;
-    capacity = 4; 
+    capacity = 4;
     cout << pageFaultsFifo(pages, n, capacity) << endl;
     cout << pageFaultsLRU(pages, n, capacity) << endl;
     cout << pageFaultsOPT(pages, n, capacity) << endl;
     cout << pageFaultsSC(pages, n, capacity) << endl;
 
-
     cout << "\nMas lapok 3 memoria igennyel" << endl;
     int pages2[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
     n = sizeof(pages2) / sizeof(pages2[0]);
-    capacity = 3; 
+    capacity = 3;
     cout << pageFaultsFifo(pages2, n, capacity) << endl;
     cout << pageFaultsLRU(pages2, n, capacity) << endl;
     cout << pageFaultsOPT(pages2, n, capacity) << endl;
